b1630ee48cea81c6ac076cf0d622d4a40e20e96d
hussainmadan2005-spec <hussainmadan2005@gmail.com>
2026-09-23 18:18:46 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "log"
+    "net"
+    "net/http"
+)
+
+func main() {
+    listener, err := net.Listen("tcp", "127.0.0.1:0")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer listener.Close()
+
+    port := listener.Addr().(*net.TCPAddr).Port
+    fs := http.FileServer(http.Dir("./static"))
+
+    http.Handle("/", fs)
+
+    log.Printf("Server running on http://localhost:%d", port)
+
+    if err := http.Serve(listener, nil); err != nil {
+        log.Fatal(err)
+    }
+}
